9a59793fba2fdab99e84361bc22cc324823e7316
Dariusz Parys <downtocode@gmail.com>
2016-12-02 16:36:26 +0100

    new docker image created

--- src/Wichtel.WebApi/dockerize.sh
+++ src/Wichtel.WebApi/dockerize.sh
@@ -4,5 +4,5 @@ dotnet build
 dotnet publish -c release -o output
 cp Dockerfile output
 cd output
-docker build -t writeline/msts2016:latest -t writeline/msts2016:0.2 .
+docker build -t writeline/msts2016:latest -t writeline/msts2016:0.3 .
 docker push writeline/msts2016
